921de322203020d0fdd3efb918fb723bbe083b0c
didi <didi@mobilogics.com.tw>
2015-05-13 12:01:34 +0800

    Initial Commit

---
+++ GLUS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GLUS
+//
+//  Created by 許佑任 on 2015/5/13.
+//  Copyright (c) 2015年 許佑任. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
